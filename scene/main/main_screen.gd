070b4321e14d4d19a620bdb76c941dadf4758119
bozar <5583771+Bozar@users.noreply.github.com>
2024-04-07 16:32:59 +0800

    Create PCs by grid coordinates.

--- scene/main/main_screen.gd
+++ scene/main/main_screen.gd
@@ -17,6 +17,11 @@ const PALETTE: Dictionary = {
     &"DEBUG": "#FE4A49",
 }
 
+const START_X: int = 50
+const START_Y: int = 54
+const STEP_X: int = 26
+const STEP_Y: int = 34
+
 
 func _ready() -> void:
     RenderingServer.set_default_clear_color(PALETTE["BACKGROUND"])
@@ -27,11 +32,25 @@ func _create_pc() -> void:
     # var pc: Sprite2D = preload("res://sprite/pc.tscn").instantiate()
     var pc: PackedScene = preload("res://sprite/pc.tscn")
     var new_pc: Sprite2D
-    var x: int = 0
+    var new_position: Vector2i = Vector2i(0, 0)
 
     for i: StringName in PALETTE.keys():
         new_pc = pc.instantiate()
-        new_pc.position = Vector2i(50 + x, 50)
+        new_pc.position = _get_position_from_coord(new_position)
         new_pc.modulate = PALETTE[i]
         add_child(new_pc)
-        x += 30
+        # print(_get_coord_from_sprite(new_pc))
+        new_position.x += 1
+
+
+func _get_position_from_coord(coord: Vector2i,
+        offset: Vector2i = Vector2i(0, 0)) -> Vector2i:
+    var new_x: int = START_X + STEP_X * coord.x + offset.x
+    var new_y: int = START_Y + STEP_Y * coord.y + offset.y
+    return Vector2i(new_x, new_y)
+
+
+func _get_coord_from_sprite(sprite: Sprite2D) -> Vector2i:
+    var new_x: int = floor((sprite.position.x - START_X) / STEP_X)
+    var new_y: int = floor((sprite.position.y - START_Y) / STEP_Y)
+    return Vector2i(new_x, new_y)
